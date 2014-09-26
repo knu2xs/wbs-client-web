d891df4b1f39e8fdbbd83231aa50cdf757f0d2d7
knu2xs <knu2xs@gmail.com>
2014-09-26 13:57:31 -0400

    new, edit and view (albeit not pretty) all working...search, lookup and copy still need to be created

---
+++ app/routes/items/new.js
@@ -0,0 +1,33 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+
+    model: function(){
+        return this.store.createRecord('item');
+    },
+
+    // if back button is pressed, clean up uncommitted edits
+    deactivate: function () {
+        var model = this.modelFor('items.edit');
+        if (model && model.get('isDirty') && !model.get('isSaving')) {
+            model.rollback();
+        }
+    },
+
+    actions: {
+
+        // exit without changing anything
+        cancel: function (item) {
+            item.rollback();
+            this.transitionTo('items.index');
+        },
+
+        // save item instance into data store and return to items list
+        save: function (item) {
+            var self = this;
+            item.save().then(function () {
+                self.transitionTo('items.index');
+            });
+        }
+    }
+});
